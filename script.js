23aa02ef944a44193dc68dc07b8aeb6d5b55d8ee
mo speight <you@example.com>
2023-12-14 18:33:51 -0500

    Fixes a bug

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const button = document.getElementById('button');
-const audioElement = document.getElementById('audio');
-
 // Disable/Enable Button
 function toggleButton() {
   button.disabled = !button.disabled;
